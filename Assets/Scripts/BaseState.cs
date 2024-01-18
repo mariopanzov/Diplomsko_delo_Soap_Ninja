71af37706d2f69e8101e218f28f0efff21f6a924
mariopanzov <m.panzovi@hotmail.com>
2024-01-18 18:06:30 +0100

    18.01.2024 save

--- Assets/Scripts/BaseState.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public abstract class BaseState
-{   
-    public abstract void onEnterState();
-    public abstract void updateState(StateManagerScript state_manager);
-    public abstract void transitionToNextState(StateManagerScript state_manager);
-}
